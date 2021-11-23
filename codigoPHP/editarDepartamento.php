3fbbdd4e063a338eddd77b2a6feda054ec42bbfb
Sonia <Sonia>
2021-11-23 10:19:42 +0100

    day 23/11/2021 add buscar working editar

--- codigoPHP/editarDepartamento.php
+++ codigoPHP/editarDepartamento.php
@@ -51,9 +51,9 @@
 
                 /* 
                  * Author: Sonia Antón Llanes
-                 * Created on: 16-noviembre-2021
+                 * Created on: 22-noviembre-2021
                  * Ejercicio 09: Mantenimiento de Departamentos
-                 *    con: busqueda, alta, bajas, modificaciones, exportar, importar...
+                 *    funcion: EDITAR
                  */
 
                     /* Importamos archivos necesarios */
@@ -68,18 +68,30 @@
 
                     /* ARRAY DE ERRORES Y ENTRADAS DEL FORMULARIO*/
                         $aErrores = array(     //Array para guardar los errores del formulario
-                            'descDepartamento' => null   //E inicializo cada elemento
+                            'descDepartamento' => null,   //E inicializo cada elemento
+                            'volumenNegocio' => null
                             );
                         $aRespuestas = array(     //Array para guardar las entradas del formulario correctas
-                            'descDepartamento' => null   //E inicializo cada elemento
+                            'descDepartamento' => null,   //E inicializo cada elemento
+                            'volumenNegocio' => null
                             );
 
                     /* FORMULARIO */
+                        /**
+                         * Si pulso CANCELAR vuelvo a Mantenimiento de Departamentos sin realizar cambio ninguno 
+                         */
+                            if (isset($_POST['cancelar'])){  //Pulso el boton cancelar
+                                echo "<a href='mtoDepartamentos.php'></a>";
+                            }
+                        /**
+                         * Si pulso ACEPTAR se valida y guardan los cambios
+                         */
                         /* VALIDACIÓN de cada entrada del formulario con la libreria de validación que importamos */
-                            if (isset($_POST['submit'])){  //Pulso el boton enviar
+                            if (isset($_POST['aceptar'])){  //Pulso el boton enviar
                                 //Valido cada campo y si hay algun error lo guardo en el array aErrores
                                     $aErrores['descDepartamento']= validacionFormularios::comprobarAlfabetico($_REQUEST['descDepartamento'], 50, 1, OPCIONAL);
-                                    //Recorro array errores y compruebo si se ha incluido algún error
+                                    $aErrores['volumenNegocio']= validacionFormularios::comprobarEntero($_REQUEST['volumenNegocio'], 100, 0, OPCIONAL);
+                                //Recorro array errores y compruebo si se ha incluido algún error
                                 foreach ($aErrores as $campo => $error){  
                                     if ($error!=null){   //si es distinto de null, hay errores
                                         $_REQUEST[$campo] = "";  //limpio el campo $_REQUEST
@@ -92,37 +104,7 @@
                                 $_REQUEST['descDepartamento'] = "";  //doy valor a $_REQUEST 
                             }
 
-                ?>
-                        <!-- MUESTRO EL FORMULARIO --> 
-                            <form name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                            <div class="table1">
-                                <table>
-                                    <tr>
-                                        <td class="dato">
-                                            <div><label for="LbDescDepartamento">Descripción del Departamento </label></div>
-                                        </td>
-                                        <td colspan="2" class="td">
-                                            <div class="datoUsu"><input type="text" name="descDepartamento" id="LbDescDepartamento"
-                                                   value="<?php  //Si no hay ningun error y se ha enviado mantenerlo
-                                                            echo $resultado = ($aErrores['descDepartamento']==NULL && isset($_REQUEST['descDepartamento'])) ? $_REQUEST['descDepartamento'] : $aRespuestas['descDepartamento']=""; 
-                                                          ?>"></div>
-                                        </td>
-                                        <td class="buscar"><input id="submit" name="submit" type="submit" value="Buscar"></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="dato"></td>
-                                        <td colspan="2" class="td">
-                                            <div class="error"><?php
-                                                    if ($aErrores['descDepartamento'] != NULL) { //si hay errores muestra el mensaje
-                                                        echo "<span style=\"color:red;\">".$aErrores['descDepartamento']."</span>"; //aparece el mensaje de error que tiene el array aErrores
-                                                    }
-                                                 ?></div>
-                                        </td>
-                                        <td class="buscar"></td>
-                                    </tr>
-                                </table>
-                            </div>
-                <?php    
+                    
 
                         /* RESULTADO con entradaOK: al comenzar y una vez enviado con entradas correctas */
                             if($entradaOK){  //Si todas las entradas son correctas
@@ -195,7 +177,60 @@
                                 finally {
                                     unset($miDB);
                                 }
-                        }    
+                        }
+                        else{
+                ?>
+                        <!-- MUESTRO EL FORMULARIO --> 
+                            <form name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                            <div class="table1">
+                                <table>
+                                    <tr>
+                                        <th colspan="2"><h3>EDITAR Departamento</h3></th>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2" class="td">
+                                            <div class="dato"><label for="LbCodDepartamento">Código del Departamento  <span class="ast">*</span></label></div>
+                                            <div class="datoUsu"><input type="text" name="codDepartamento" id="LbCodDepartamento" readonly="readonly" style="background: lightgray"
+                                                   value="<?php  //Mantenemos el valor - primary key
+                                                            echo $valor = $_SERVER['codDepartamento'];?>"></div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2" class="td">
+                                            <div class="dato"><label for="LbDescDepartamento">Departamento  <span class="ast">*</span></label></div>
+                                            <div class="datoUsu"><input type="text" name="descDepartamento" id="LbDescDepartamento"
+                                                   value="<?php  //Si no hay ningun error y se ha enviado mantenerlo
+                                                            echo $resultado = ($aErrores['eDescDepartamento']==NULL && isset($_POST['descDepartamento'])) ? $_POST['descDepartamento'] : ""; 
+                                                          ?>"></div>
+                                            <div class="error"><?php
+                                                    if ($aErrores['eDescDepartamento'] != NULL) { //si hay errores muestra el mensaje
+                                                        echo "<span style=\"color:red;\">".$aErrores['eDescDepartamento']."</span>"; //aparece el mensaje de error que tiene el array aErrores
+                                                    }
+                                                 ?></div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2" class="td" colspan="2">
+                                            <div class="dato"><label for="LbVolumenNegocio">Volumen de Negocio </label></div>
+                                            <div class="datoUsu"><input type="text" name="volumenNegocio" id="LbVolumenNegocio"
+                                                   value="<?php  //Si no hay ningun error y se ha enviado mantenerlo
+                                                            echo $resultado = ($aErrores['eVolumenNegocio']==NULL && isset($_POST['volumenNegocio'])) ? $_POST['volumenNegocio'] : ""; 
+                                                          ?>"></div>
+                                            <div class="error"><?php
+                                                    if ($aErrores['eVolumenNegocio'] != NULL) { //si hay errores muestra el mensaje
+                                                        echo "<span style=\"color:red;\">".$aErrores['eVolumenNegocio']."</span>"; //aparece el mensaje de error que tiene el array aErrores
+                                                    }
+                                                 ?></div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th><input type="submit" id="aceptar" name="aceptar" value="ACEPTAR"></th>
+                                        <th><input type="submit" id="cancelar" name="cancelar" value="CANCELAR"></th>
+                                    </tr>
+                                </table>
+                            </div>
+                <?php
+                        }
 
                 ?>    
 
@@ -212,9 +247,9 @@
                     <li class="ftexto"><a href="../index.php">&copy 2020-21. Sonia Anton LLanes</a></li>
                     <li>
                         
-                        <a class="maxMedia" href="doc/curriculum_SALL.pdf" target="_blank"><img src="../webroot/images/CV.png" alt="imagen_CV"></a>
-                        <a class="maxMedia" href=""><img src="../webroot/images/linkedin.png" alt="imagen_linkedIn"></a>
-                        <a class="maxMedia" href="https://github.com/SoniaALLSauces/proyectoTema4" target="_blank"><img src="../webroot/images/github.png" alt="imagen_github"></a>
+                        <a class="maxMedia" href="doc/curriculum_SALL.pdf" target="_blank"><img src="webroot/images/CV.png" alt="imagen_CV"></a>
+                        <a class="maxMedia" href=""><img src="webroot/images/linkedin.png" alt="imagen_linkedIn"></a>
+                        <a class="maxMedia" href="https://github.com/SoniaALLSauces/proyectoTema4" target="_blank"><img src="webroot/images/github.png" alt="imagen_github"></a>
                     </li>
                 </ul>
             </nav>
